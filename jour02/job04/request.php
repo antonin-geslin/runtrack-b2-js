ecec6ea0804c54c8233af8b262ecbbae49547bda
Antonin Geslin Dugué <latouillette@MacBook-Air-de-Antonin.local>
2023-09-08 09:06:02 +0200

    jour02

--- jour02/job04/request.php
+++ jour02/job04/request.php
@@ -1,5 +1,8 @@
 <?php 
-    function my_insert_student(string $email, string $fullname, string $gender, int $gradeId, DateTime $birthdate) : bool{
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+function my_insert_student(string $email, string $fullname, string $gender, int $gradeId, DateTime $birthdate) : bool{
         $formattedBirthdate = $birthdate->format('Y-m-d');
         $bdd = new PDO('mysql:host=localhost;dbname=lp_official;charset=utf8', 'root', 'root');
         $requete = $bdd->prepare("INSERT INTO student (grade_id, email, fullname, birthdate, gender) VALUES (:grade_id, :email, :fullname, :birthdate, :gender)");
@@ -15,10 +18,4 @@
         return false;   
     }
 
-    $email = $_POST['student-mail'];
-    $fullname = $_POST['student-fullname'];
-    $gender = $_POST['student-gender'];
-    $grade = $_POST['student-grade'];
-    $birthdate = new DateTime($_POST['student-birthdate']);
-    my_insert_student($email, $fullname, $gender, $grade, $birthdate);
 ?>
--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -1,32 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-function myRegisterStudent(form) {
-    var email = form.querySelector("#student-mail").value;
-    console.log(email);
-    var fullname = form.querySelector("#student-fullname").value;
-    console.log(fullname);
-    var gender = form.querySelector("#student-gender").value;
-    console.log(gender);
-    var grade = form.querySelector("#student-grade").value;
-    console.log(grade);
-    var birthdate = form.querySelector("#student-birthdate").value;
-    console.log(birthdate);
+    function myRegisterStudent(form) {
+        var formData = new FormData(form);
 
-    var formData = new FormData(form);
+        fetch("request.php", {
+            method: "POST",
+            body: formData
+        })
+        .then(function(response) {
+            if (!response.ok) {
+                console.error("Réponse non OK :", response);
+                throw new Error("Erreur lors de la requête.");
+            }
+        })
+        .then(function(data) {
+            // Traitez la réponse ici
+            if (data === "success") {
+                // L'insertion a réussi
+                alert("Étudiant enregistré avec succès !");
+            } else {
+                // L'insertion a échoué, data contiendra un message d'erreur
+                alert("Erreur lors de l'enregistrement de l'étudiant : " + data);
+            }
+        })
+        .catch(function(error) {
+            // Gestion des erreurs
+            alert("Erreur : " + error.message);
+        });
 
-    fetch("request.php", {
-        method: "POST",
-        body: formData
-    })
-    .then(function(response) {
-        // Traitement de la réponse de request.php
-    })
-    .catch(function(error) {
-        // Gestion des erreurs
-    });
-
-}
-    var submitButton = document.getElementById("submit-btn");
-    var form = document.getElementById("form-add-student");
-    submitButton.addEventListener("click", myRegisterStudent(form));
+    }
+        var submitButton = document.getElementById("submit-btn");
+        var form = document.getElementById("form-add-student");
+        submitButton.addEventListener("click", function(event) {
+            event.preventDefault();
+            myRegisterStudent(form);
+        });
+        
 });
