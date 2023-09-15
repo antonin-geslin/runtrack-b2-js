a12180b21df59edc28fe0ef028cf206dcfec56bb
Antonin Geslin Dugué <115146768+antonin-geslin@users.noreply.github.com>
2023-09-15 12:16:21 +0200

    Delete jour01 directory

--- jour01/job04/script.js
+++
@@ -1,9 +0,0 @@
-function myArraySum(array) {
-    result = 0;
-    for (var l in array) {
-        result += array[l];
-    }
-    return result;
-}
-
-console.log(myArraySum([4, 24, 52, 14, 32, 56])) === 182;
